05cef314b3b1b61dd492e3e3f9a295b07c775798
Alexei Dudarev <alexei.dudarev@gmail.com>
2019-10-30 22:04:32 +0200

    Prints class created
    print function for aarray added

--- Lesson05/src/lesson05/Functions.java
+++ Lesson05/src/lesson05/Functions.java
@@ -1,16 +1,39 @@
 package lesson05;
 
-public class Functions {
+
+
+public class Functions extends Prints {
 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		int x = 0;
-=======
-		//Hello java
->>>>>>> 60ad2d7842780f4b5a67f8f5ee9627f2e8feea60
+
+		int [] arr = {-1,2,3,-45,1};
+		printMax(arr);
+		
+		
+		
+		
+		
+		
+		/*
+		int sum = 0;
+		for(int i = 0; i < 10 ; i++) {
+			sum = sum + 1;
+			if (i == 2) {
+				continue;
+			}
+			if (i == 9) {
+				break;
+			}
+			
+			System.out.print(i+" ,");
+		}
+		*/
 
 	}
+	
+	
+	
 
 }
---
+++ Lesson05/src/lesson05/Prints.java
@@ -0,0 +1,27 @@
+package lesson05;
+
+public class Prints {
+	
+	public void printArr(int [] arr) {
+		for(int x = 0;x < arr.length;x++) {
+			System.out.print(arr[x]);
+		}
+	}
+	public static void printMax(int[] arr) {
+		int max= 0;
+		
+		int min = arr[0];
+		for(int x = 0;x < arr.length;x++) {
+			if (arr[x]>max) {
+				max =arr[x];
+			}
+			if (arr[x]<min) {
+				min =arr[x];
+			}
+			
+		}
+		System.out.println("the max num i :" + max);
+		System.out.println("the min num i :" + min);
+	}
+
+}
